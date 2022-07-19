84b7121b6ee76de3034214a19c8c6c2c09270ebe
MykytaKrupskyi <109085163+MykytaKrupskyi@users.noreply.github.com>
2022-07-19 21:47:57 +0200

    Add files via upload
    the first test for generation

---
+++ passwords.py
@@ -0,0 +1,7 @@
+import secrets
+import string
+
+alphabet = string.ascii_letters + string.digits
+password = ''.join(secrets.choice(alphabet) for i in range(20))  # for a 20-character password
+
+print(password)
