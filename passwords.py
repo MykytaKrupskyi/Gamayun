7148f56096fc33268c6cc24578f813d6ba002860
MykytaKrupskyi <109085163+MykytaKrupskyi@users.noreply.github.com>
2022-07-22 20:45:22 +0200

    Added list method and remade inappropriate part
    List method was added as printing of dictionary.
    And the exec() usage was changed locals()[name]... on line 65

--- passwords.py
+++ passwords.py
@@ -2,16 +2,19 @@ import secrets
 import string
 
 class Password:
-    def __init__(self, style, size):
-
+    passwords = {}
+    def __init__(self, style, size, name=None):
+        
+        # assignment of variables
         self.style= style
         self.size = size
+        self.name = name
 
         # decision-making statement for password type
         if self.style == "letters":
             sample = string.ascii_letters 
         elif self.style == "numbers":
-            sample = string.string.digits
+            sample = string.digits
         else:
             sample = string.ascii_letters + string.digits
 
@@ -19,39 +22,49 @@ class Password:
         password = ''.join(secrets.choice(sample) for i in range(self.size))
         self.password = password
 
+        # adding values to dictionary
+        Password.passwords[name] = password 
+
 class Interface():
     def __init__(self):
         self.string = str()
-        d = {} # saves the password names 
 
-        print("""Hello, this is Gamayun!
-I'm password manager.
-Type 'help' to see what I can""")
+        print("Hello, this is Gamayun!\nI'm password manager.\nType 'help' to see what I can")
 
+        # Repeating cycle for taking commands from user
         while self.string != "exit":
-            self.string = input(">")
+            self.string = input("> ")
         
             # Help menu
             if self.string == "help":
-                print("""The options are:
-help - to show this option
-new - to create new password """)
+                print("""The options are:\nhelp - to show this option\nnew - to create new password\nlist - to list all passwords""")
 
-        # Creating new password
+            # Creating new password
             elif self.string == "new":
-                name = input("Enter new password's name: ")
-                length = int(input("Enter new password's length: "))
-                style = input("Enter new password's style(mix, letters, numbers): ")
-                
-                while True:
-                    if style == "mix" or "letters" or "numbers":
-                        break
-                    else:
-                        style = input("Enter new password's style(mix, letters, numbers): ")
+                self._new()
 
-                exec("{0} = Password(style, length)".format(name)) # temporary solution!!!
+            # Listing all passwords
+            elif self.string == "list":
+                self._list()
 
             else:
                 pass
+
+    def _new(self):
+
+        name = input("Enter new password's name: ")
+        length = int(input("Enter new password's length: "))
+        style = input("Enter new password's style(mix, letters, numbers): ")
+        
+        while True:
+            if style == "mix" or "letters" or "numbers":
+                break
+            else:
+                style = input("Enter new password's style(mix, letters, numbers): ")
+        
+        locals()[name] = Password(style, length, name)  
+
+    def _list(self):
+        [print(key,':',value) for key, value in Password.passwords.items()]
 
 defined = Interface()
