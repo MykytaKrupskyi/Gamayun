e7a13f542a2401698c1985c6cc69084591c3bfa0
MykytaKrupskyi <109085163+MykytaKrupskyi@users.noreply.github.com>
2022-07-23 20:10:47 +0200

    Added functions:  change of password & deletion of it
    I'm not okay with how code looks like & work, so I'll make a full rework of whole.

--- passwords.py
+++ passwords.py
@@ -1,19 +1,19 @@
 import secrets
 import string
 
+def name_of_global_obj(xx):
+    return [objname for objname, oid in globals().items()
+            if id(oid)==id(xx)][0]
+
 class Password:
     passwords = {}
     # Initialization of password
-    def __init__(self, style, size, name=None):
+    def __init__(self, style, size, name):
         self.name = name
 
         # generates new value and assigns it
         self.password = Password._generate(self, style, size)
         Password.passwords[name] = self.password 
-
-    # Changing of password
-    def _change(self, style, size):
-        self.password = new
 
     # Generation of password
     def _generate(self, style, size):
@@ -31,11 +31,28 @@ class Password:
         # the generating line itself
         password = ''.join(secrets.choice(sample) for i in range(self.size))
         return password
+        
+        print(self)
+
+   # Changing of password
+    def _change(self, new_password = None, style = None, size = None):
+        if new_password == None:
+            self.password = Password._generate(self, style, size) 
+        else:
+            self.password = new_password
+        Password.passwords[self.name] = self.password 
+        
+
+    # Deleting of password
+    def _delete(name):
+        del name
+        del passwords[name]
 
 class Interface():
     def __init__(self):
         self.string = str()
-        print("\nHello, this is Gamayun!\nI'm password manager.\nType 'help' to see what I can\n")
+        print("\
+\nHello, this is Gamayun!\nI'm password manager.\nType 'help' to see what I can\n")
         self._input()
 
     # Repeating cycle for taking commands from user
@@ -44,7 +61,10 @@ class Interface():
             self.string = input("> ")
         
             if self.string == "help": # help tips
-                print("\nThe options are:\n\nhelp - to show this option\nnew - to create new password\nlist - to list all passwords\nchange - to change password\nremove - to delete password \n")
+                print("\nThe options are:\
+ \n\nhelp - to show this option\nnew - to create new password\
+ \nlist - to list all passwords\nchange - to change password\
+ \nremove - to delete password \n")
 
             elif self.string == "new": # creates new password
                 self._new()
@@ -55,14 +75,17 @@ class Interface():
             elif self.string == "change": # changes password
                 self._change()
 
+            elif self.string == "remove": # remove password
+                self._delete()
             else:
                 pass
     
     # Creating new password
     def _new(self):
-
+        
+        entered_password = {}
         name = input("\nEnter new password's name: ")
-        length = int(input("Enter new password's length: "))
+        size = int(input("Enter new password's length: "))
         style = input("Enter new password's style(mix, letters, numbers): ")
         print()
 
@@ -71,13 +94,39 @@ class Interface():
                 break
             else:
                 style = input("Enter new password's style(mix, letters, numbers): ")
-        
-        locals()[name] = Password(style, length, name)
 
-    # Lists all the passwords
+        # assignment of password
+        entered_password[name] = entered_password.get(name, Password(style, size, name = name))
+         
+        dir({Password})
+    # List of all the passwords
     def _list(self):
         print()
         [print(key,':',value) for key, value in Password.passwords.items()]
         print()
 
+    # Changing of password
+    def _change(self):
+        entered_password = {}
+        if Password.passwords: # handling passwords absence 
+            self._list()
+            name = input("Which one should be changed: ")
+
+            while not (name in Password.passwords.keys()): # checking for presence
+                name = input(f"There is no {name}. Enter again: ")
+                
+            do = input("Do you want gamayun to generate new password?(Y/n): ")
+
+            if do == "Y":
+                size = int(input("Enter new password's length: "))
+                style = input("Enter new password's style(mix, letters, numbers): ")
+                entered_password[name] = entered_password.get(name, Password(style, size, name = name))
+                exec(f"{name}._change(None, style, size, name)") # temporary solution
+            else:
+                password = input("Enter new password: ")
+                exec(f"{name}._change(password)")
+
+        else: print("You have no passwords to change!")
+
+# Main part of program
 defined = Interface()
